1a8abd50144f9c60fc7b4a71bb565bab63c7eb6c
Sombrer0Dev <tumakaelfufaka@gmail.com>
2024-06-21 23:42:16 +0300

    final for the day 2

--- home-manager/scripts/shell-tricks.nix
+++ home-manager/scripts/shell-tricks.nix
@@ -4,22 +4,8 @@
     echo $page | awk 'BEGIN {ORS=" "}; {print $2} {print $1}'| tr -d '()' | xargs man
   '';
   direnv-switch = pkgs.writeShellScriptBin "direnv-switch" ''
-    set -euo pipefail
-
-    env_name=$1
-    env_file=.env.''${env_name}
-
-    if [[ ! -f $env_file ]]; then
-      echo "env $env_name not found" >&2
-      exit 1
-    fi
-
-    cat <<NEW_ENVRC > .envrc
-    log Loading environment "$env_name"
-    dotenv "$env_file"
-    NEW_ENVRC
-
-    direnv allow .
+    export APP_ENV="$1"
+    direnv allow
   '';
 in {
   home.packages = [ bman direnv-switch ];
