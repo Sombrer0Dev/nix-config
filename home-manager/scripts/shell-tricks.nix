14bd71a092344da3f2f8b0851952c45ef68317e9
Sombrer0Dev <tumakaelfufaka@gmail.com>
2024-06-21 23:52:44 +0300

    final for the day 2

--- home-manager/scripts/shell-tricks.nix
+++ home-manager/scripts/shell-tricks.nix
@@ -3,10 +3,6 @@
     page=$(man -k . | fzf-tmux --nth 1,2)
     echo $page | awk 'BEGIN {ORS=" "}; {print $2} {print $1}'| tr -d '()' | xargs man
   '';
-  direnv-switch = pkgs.writeShellScriptBin "direnv-switch" ''
-    export APP_ENV="$1"
-    direnv allow
-  '';
 in {
-  home.packages = [ bman direnv-switch ];
+  home.packages = [ bman ];
 }
