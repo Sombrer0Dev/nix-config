b1fea764f6723ae8129ecf71c4cdab2a5514dd1e
Artem Sokolov <sombrer01@gmail.com>
2024-07-25 23:50:39 +0300

    nvim

--- home-manager/wireguard.nix
+++ home-manager/wireguard.nix
@@ -1,4 +1,11 @@
-{pkgs, ...}:{
-  home.packages = with pkgs; [ wireguard-go wireguard-tools ];
+{pkgs, ...}: let
+  wgu = pkgs.writeShellScriptBin "wgu" ''
+    ls -1 /etc/wireguard | sed -e 's/\..*$//' | fzf | xargs wg-quick up
+  '';
+  wgd = pkgs.writeShellScriptBin "wgd" ''
+    ls -1 /etc/wireguard | sed -e 's/\..*$//' | fzf | xargs wg-quick down
+  '';
+in{
+  home.packages = with pkgs; [ wireguard-go wireguard-tools wgu wgd];
 }
 
--- nvim/lua/core/plugins/harpeek.lua
+++ nvim/lua/core/plugins/harpeek.lua
@@ -1,68 +1,83 @@
 ---@param path string
 ---@return string?
 local function split_oil_dir(path)
-  if path:sub(1, 6) ~= 'oil://' then
-    return nil
-  end
+	if path:sub(1, 6) ~= "oil://" then
+		return nil
+	end
 
-  if path:sub(-1, -1) == '/' then
-    return path:sub(7, -2)
-  else
-    return path:sub(7, -1)
-  end
+	if path:sub(-1, -1) == "/" then
+		return path:sub(7, -2)
+	else
+		return path:sub(7, -1)
+	end
 end
 
 return {
-  'WolfeCub/harpeek.nvim',
-  -- dir = '~/Workspace/personal/lua/harpeek.nvim/',
-  event = {'BufReadPre', 'BufNewFile'},
-  config = function()
-    local harpeek = require 'harpeek'
-    harpeek.setup {
-      format = function(path, index)
-        local oil_path = split_oil_dir(path)
-        local suffix = ''
-        if oil_path then
-          path = oil_path
-          suffix = '/'
-        end
+	"WolfeCub/harpeek.nvim",
+	-- dir = '~/Workspace/personal/lua/harpeek.nvim/',
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local harpeek = require("harpeek")
+		harpeek.setup({
+			format = function(path, index)
+				local oil_path = split_oil_dir(path)
+				local suffix = ""
+				if oil_path then
+					path = oil_path
+					suffix = "/"
+				end
 
-        -- TODO: This logic is pretty opaque. I could probably do something nicer.
-        local formatted_line = ''
-        formatted_line = vim.fn.fnamemodify(path, ':t') .. suffix
-        if index == 1 then
-          index = '{n}'
-        elseif index == 2 then
-          index = '{t}'
-        elseif index == 3 then
-          index = '{l}'
-        elseif index == 4 then
-          index = '{p}'
-        else
-          index = '[' .. index .. ']'
-        end
-        return index .. ' ' .. formatted_line
-      end,
-      winopts = {
-        row = vim.api.nvim_win_get_height(0) - 15,
-        border="solid"
-      },
-    }
-    harpeek.open()
+				-- TODO: This logic is pretty opaque. I could probably do something nicer.
+				local formatted_line = ""
+				formatted_line = vim.fn.fnamemodify(path, ":t") .. suffix
+				if index == 1 then
+					index = "{n}"
+				elseif index == 2 then
+					index = "{t}"
+				elseif index == 3 then
+					index = "{l}"
+				elseif index == 4 then
+					index = "{p}"
+				else
+					index = "[" .. index .. "]"
+				end
+				return index .. " " .. formatted_line
+			end,
+			winopts = {
+				row = vim.api.nvim_win_get_height(0) - 15,
+				border = "solid",
+			},
+		})
+		harpeek.open()
 
-    require('legendary').keymaps {
-      {
-        '<leader>h',
-        function()
-          harpeek.toggle {
-            winopts = {
-              row = vim.api.nvim_win_get_height(0) - 15,
-              border="none"
-            },
-          }
-        end,
-        description = 'Harpeek',
-      },
-    }
-  end,
+    local i = 1
+		vim.api.nvim_create_autocmd("VimResized", {
+			callback = function()
+        i = i+1
+				vim.print("resized", i)
+        harpeek.close()
+				harpeek.open({
+					winopts = {
+						row = vim.api.nvim_win_get_height(0) - 15,
+						border = "none",
+					},
+				})
+			end,
+		})
+
+		require("legendary").keymaps({
+			{
+				"<leader>h",
+				function()
+					harpeek.toggle({
+						winopts = {
+							row = vim.api.nvim_win_get_height(0) - 15,
+							border = "none",
+						},
+					})
+				end,
+				description = "Harpeek",
+			},
+		})
+	end,
 }
