1106c6a56a2ac41f357fd944e0585252e8c61c43
Artem Sokolov <sombrer01@gmail.com>
2024-08-09 00:04:06 +0300

    and stuff

--- home-manager/foot.nix
+++ home-manager/foot.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.foot = {
     enable = true;
@@ -16,24 +16,24 @@
         foreground = "ffffff";
 
         ## Normal/regular colors (color palette 0-7)
-        regular0="16181a";  # black
-        regular1="ff6e5e";  # red
-        regular2="5eff6c";  # green
-        regular3="f1ff5e";  # yellow
-        regular4="5ea1ff";  # blue
-        regular5="bd5eff";  # magenta
-        regular6="5ef1ff";  # cyan
-        regular7="ffffff";  # white
+        regular0 = "16181a"; # black
+        regular1 = "ff6e5e"; # red
+        regular2 = "5eff6c"; # green
+        regular3 = "f1ff5e"; # yellow
+        regular4 = "5ea1ff"; # blue
+        regular5 = "bd5eff"; # magenta
+        regular6 = "5ef1ff"; # cyan
+        regular7 = "ffffff"; # white
 
         ## Bright colors (color palette 8-15)
-        bright0="3c4048";   # bright black
-        bright1="ff6e5e";   # bright red
-        bright2="5eff6c";   # bright green
-        bright3="f1ff5e";   # bright yellow
-        bright4="5ea1ff";   # bright blue
-        bright5="bd5eff";   # bright magenta
-        bright6="5ef1ff";   # bright cyan
-        bright7="ffffff";   # bright white
+        bright0 = "3c4048"; # bright black
+        bright1 = "ff6e5e"; # bright red
+        bright2 = "5eff6c"; # bright green
+        bright3 = "f1ff5e"; # bright yellow
+        bright4 = "5ea1ff"; # bright blue
+        bright5 = "bd5eff"; # bright magenta
+        bright6 = "5ef1ff"; # bright cyan
+        bright7 = "ffffff"; # bright white
 
       };
     };
