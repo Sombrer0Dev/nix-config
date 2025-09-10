346cb78d3fc252bee98073ba51121e6eb607bab8
Artem Sokolov <smbrer1@gmail.com>
2025-09-10 11:28:19 +0300

    vimrc

--- modules/home-manager/work.nix
+++ modules/home-manager/work.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 let
-  vpnpt= pkgs.writeShellScriptBin "vpnpt" ''
-    sudo openconnect vpn.mtt.ru --user=avsokolov --authgroup=MTT_RemoteAccess
-  '';
+  # vpnpt= pkgs.writeShellScriptBin "vpnpt" ''
+  #   sudo openconnect vpn.mtt.ru --user=avsokolov --authgroup=MTT_RemoteAccess
+  # '';
 in
 {
   home.packages = [
-    vpnpt
+    # vpnpt
     pkgs.mattermost-desktop
     pkgs.networkmanager-openvpn
-    pkgs.openvpn
+    pkgs.openvp
   ];
 }
